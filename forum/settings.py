8f5f960ffe39a816f5c586b43b958b290c9e7a71
Gugasuave <113999433+Gugasuave@users.noreply.github.com>
2023-05-26 21:42:33 -0300

    Update settings.py

--- forum/settings.py
+++ forum/settings.py
@@ -117,8 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+# settings.py
 STATIC_URL = '/static/'
-STATIC_ROOT = 'staticfiles'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 LOGIN_REDIRECT_URL = '/forumapp'
 LOGOUT_REDIRECT_URL = '/forumapp'
