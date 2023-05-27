2ab57cb46246d7df11970a701c2ce3684a8bb2df
Gugasuave <113999433+Gugasuave@users.noreply.github.com>
2023-05-27 09:44:03 -0300

    Update settings.py

--- forum/settings.py
+++ forum/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g=3kj&$zc9fz*_0ahe6u98xo!8jdpq27j$%eme*mj&v5hgunun'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
